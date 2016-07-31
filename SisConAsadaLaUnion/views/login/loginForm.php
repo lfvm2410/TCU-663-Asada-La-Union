f77301148279711e71104b22a56eb58af9d9caec
lfvm2410 <luis_f_94@hotmail.com>
2016-07-31 04:01:17 -0600

    Validación cliente/servidor de registrar cliente
    -Validación front/back terminada

--- SisConAsadaLaUnion/views/login/loginForm.php
+++ SisConAsadaLaUnion/views/login/loginForm.php
@@ -28,25 +28,25 @@
  	<a href="#"><h5 class="text-center" style="margin-top:5.6%;">¡Olvide mi contraseña!</h5></a> 
 </div>
 <footer class="footer">
-	<div class="container" style="color:#FFFFFF; margin-top:1%;">
-		<div class="col-md-4">
+	<div class="container pie">
+		<div class="col-md-4 pieCol">
 		    <h4>ASADA La Unión, Guápiles</h4>
 		    <hr>
-			<a href="#"><h5 style="color:#FFFFFF;">Quiénes somos</h5></a>
-			<h5 style="color:#FFFFFF;">&copy Copyright 2016. Todos los derechos reservados.</h5>
+			<a href="#"><h5>Quiénes somos</h5></a>
+			<h5 style="">&copy Copyright 2016. Todos los derechos reservados.</h5>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-4 pieCol">
 			<h4>Redes sociales y contacto</h4>
 			<hr>
 			<a href="#"><img class="col-md-3 img-responsive img-round" src="<?php print URL;?>public/assets/images/FbLogo.png" width="38px"/></a>
 			<a href="#"><img class="col-md-3 img-responsive img-round" src="<?php print URL;?>public/assets/images/GoogleLogo.png" width="38px"/></a>
 			<a href="#"><img class="col-md-3 img-responsive img-round" src="<?php print URL;?>public/assets/images/TelefonoLogo.png" width="38px"/></a>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-4 pieCol">
 			<h4>Desarrolladores</h4>
 			<hr>
-			<a href="#"><h5 style="color:#FFFFFF;">Alexander Gamboa Salazar</h5></a>
-			<a href="#"><h5 style="color:#FFFFFF;">Luis Fernando Vargas Ménendez</h5></a>
+			<a href="#"><h5>Alexander Gamboa Salazar</h5></a>
+			<a href="#"><h5>Luis Fernando Vargas Ménendez</h5></a>
 		</div>
 	</div>
 </footer>
