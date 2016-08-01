f33083fc01f243acfc3c4c68c34080aaaf1a67be
lfvm2410 <luis_f_94@hotmail.com>
2016-07-31 22:15:31 -0600

    Optimización de código
    -Corrección de detalles en la validación de el form de registrar cliente
    -Métodos reutilizados y más dinámicos

--- SisConAsadaLaUnion/libs/validation/personaValidation.php
+++ SisConAsadaLaUnion/libs/validation/personaValidation.php
@@ -4,13 +4,11 @@
 	*Clase encargada de contener todos los métodos necesarios para la validación de datos referentes a personas
 	*/
 	
-	class personaValidation{
-
-		private $personaData;
+	class personaValidation extends generalValidation{
 		
 		public function __construct(){
 
-			$this->personaData = new personaData();
+			parent::__construct();
 		}
 
 		/*
@@ -19,13 +17,15 @@
 
 		// A tomar en cuenta: Máximo de 16 caracteres, solo números enteros, no nulo y unique
 
-		public function validarCedula($cedula){
+		public function validarCedula($cedula, personaData $personaData){
 
 			$estadoCedula = false;
 
-			if (strlen($cedula) <= 16 && !is_null($cedula) && !empty($cedula) && preg_match("/^[0-9]*$/", $cedula)) {
+			$patternCedula = "/^[0-9]*$/";
+
+			if ($this->validarCamposTextoRegex($cedula,16,$patternCedula)) {
 				
-				if (!$this->personaData->comprobarExistenciaCedula($cedula)) {
+				if (!$personaData->comprobarExistenciaCedula($cedula)) {
 
 					$estadoCedula = true;
 				}
@@ -36,76 +36,20 @@
 		}
 
 		/*
-        // Metodo encargado de validar el atributo cédula para consulta select con ajax
-		*/
-
-		// A tomar en cuenta: Máximo de 16 caracteres, solo números enteros, no nulo
-
-		public function validarCedulaAjax($cedula){
-
-			$estadoCedula = false;
-
-			if (strlen($cedula) <= 16 && !is_null($cedula) && !empty($cedula) && preg_match("/^[0-9]*$/", $cedula)) {
-
-				$estadoCedula = true;
-				
-			}
-
-			return $estadoCedula;
-		}
-
-		/*
-        //Metodo encargado de validar el atributo nombre y apellidos
-		*/
-
-		// A tomar en cuenta: Máximo de 30 caracteres, no nulo
-
-		public function validarNombreApellidos($nombreApellidos){
-
-			$estadoNombreApellidos = false;
-
-			if (strlen($nombreApellidos) <= 30 && !is_null($nombreApellidos) && !empty($nombreApellidos)) {
-				
-				$estadoNombreApellidos = true;
-			}
-
-			return $estadoNombreApellidos;
-		}
-
-		/*
-        // Metodo encargado de validar el atributo del correo electrónico
-		*/
-
-		// A tomar en cuenta: Máximo de 30 caracteres, pattern de correo, no nulo
-
-		public function validarCorreoElectronicoAjax($correoElectronico){
-
-			$estadoCorreoElectronico = false;
-
-			if (strlen($correoElectronico) <= 30 && !is_null($correoElectronico) && !empty($correoElectronico) && 
-				preg_match("/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/", $correoElectronico)) {
-
-				$estadoCorreoElectronico = true;
-			
-			}
-
-			return $estadoCorreoElectronico;
-		}
-
-		/*
         // Metodo encargado de validar el atributo del correo electrónico para consulta select con ajax
 		*/
 
 		// A tomar en cuenta: Máximo de 30 caracteres, pattern de correo, no nulo y unique
 
-		public function validarCorreoElectronico($correoElectronico){
+		public function validarCorreoElectronico($correoElectronico, personaData $personaData){
 
 			$estadoCorreoElectronico = false;
 
-			if (strlen($correoElectronico) <= 30 && !is_null($correoElectronico) && !empty($correoElectronico) && 
-				preg_match("/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/", $correoElectronico)) {
+			$patternCorreoElectronico = "/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/";
+
+			if ($this->validarCamposTextoRegex($correoElectronico,30,$patternCorreoElectronico)) {
 				
-				if (!$this->personaData->comprobarExistenciaCorreoElectronico($correoElectronico)) {
+				if (!$personaData->comprobarExistenciaCorreoElectronico($correoElectronico)) {
 
 					$estadoCorreoElectronico = true;
 				}
@@ -113,24 +57,6 @@
 			}
 
 			return $estadoCorreoElectronico;
-		}
-
-		/*
-        // Metodo encargado de validar el atributo de dirección
-		*/
-
-		// A tomar en cuenta: Máximo 300 caracteres, no nulo
-
-		public function validarDireccion($direccion){
-
-			$estadoDireccion = false;
-
-			if (strlen($direccion) <= 300 && !is_null($direccion) && !empty($direccion)) {
-				
-				$estadoDireccion = true;
-			}
-
-			return $estadoDireccion;
 		}
 
 	}
