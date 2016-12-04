6adbf8c3889941c8332f431c5d465ce070319af6
DARAYAM3108 <d_araya31m@hotmail.com>
2016-12-03 20:09:23 -0600

    Cambios hasta Sab 3 Dic 2016

--- SisConAsadaLaUnion/views/index/index.php
+++ SisConAsadaLaUnion/views/index/index.php
@@ -1,5 +1,12 @@
-<?php 
-
-echo "Soy la vista del controlador del index";
-
+<?php include COMPONENTS.'header.php';?>
+	<link href="<?php print URL;?>public/css/consultarInformacionClientes.css" rel="stylesheet"/>
+    <script src="<?php print URL;?>public/js/esimakin-twbs-pagination-4a2f5ff/jquery.twbsPagination.min.js" type="text/javascript"></script>
+</head>
+<body>
+<?php include COMPONENTS.'encabezado.php';?>
+<?php
+   echo "Soy la vista del controlador del index";
 ?>
+<?php include COMPONENTS.'pie.php';?>
+</body>
+</html>
