9a61f24398c96ec9d961d010ef6547852e34f04e
DARAYAM3108 <d_araya31m@hotmail.com>
2016-12-04 00:44:59 -0600

    Revert "Cambio configuracion"
    This reverts commit 489494f337acf7dddcca429f513d90de52e2e6d1.

---
+++ SisConAsadaLaUnion/config.php
@@ -0,0 +1,21 @@
+<?php
+
+	define('URL', '/SisConAsadaLaUnion/');
+	define('LIBS', 'libs/');
+	define('DOMAIN', 'libs/domain/');
+	define('VALIDATION', 'libs/validation/');
+	define('LOGIC', 'logic/');
+	define('MODELS', 'models/');
+	define('COMPONENTS', 'views/components/');
+	
+	define('DB_HOST', 'db4free.net');
+	define('DB_USER', 'devasada');
+	define('DB_PASS', 'lokeraPerrito69');
+	define('DB_BASE', 'bdasada_launion');
+
+	/*define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '1234');
+	define('DB_BASE', 'BDASADA_LaUnion');*/
+	
+?>
