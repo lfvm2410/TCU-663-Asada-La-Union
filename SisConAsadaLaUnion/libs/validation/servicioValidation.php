399fe8b399ce50620fe83c93011d58eb964f9134
lfvm2410 <luis_f_94@hotmail.com>
2017-01-27 11:05:51 -0600

    Corrección visual y tarifas

--- SisConAsadaLaUnion/libs/validation/servicioValidation.php
+++ SisConAsadaLaUnion/libs/validation/servicioValidation.php
@@ -11,6 +11,23 @@
 
 		}
 
-		
+		/*
+		//Metodo encargado de validar el tipo de servicio
+		*/
+
+		public function validarTipoServicio($tipoServicio){
+
+			$estadoTipoServicio = false;
+
+			if (strcmp($tipoServicio, "Domipre") == 0 || strcmp($tipoServicio, "Endomipre") == 0) {
+
+				$estadoTipoServicio = true;
+				
+			}
+
+			return $estadoTipoServicio;
+
+		}
+
 	}
 ?>
